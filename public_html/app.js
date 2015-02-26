e46f065d27f91e083377b0870c4983a613347ee9
Evelyn Hathaway <git@evelynhathaway.com>
2015-02-26 01:25:41 -0800

    Index.html without LumX

--- public_html/app.js
+++
@@ -1,6 +0,0 @@
-angular.module('material-colorize', ['ngRoute', 'lumx', 'hljs', 'Services'])
-.controller('AppController', function(LxNotificationService) {
-    $scope.dropdownAlert = function() {
-            LxNotificationService.info('Clicked!');
-    };
-});
