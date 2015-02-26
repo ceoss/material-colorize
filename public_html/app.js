449c51eab0a3ae36b058b71e3daf0c748b3db803
Evelyn Hathaway <git@evelynhathaway.com>
2015-02-26 00:24:51 -0800

    Test Commit

--- public_html/app.js
+++ public_html/app.js
@@ -1,19 +1,6 @@
-/* global angular */
-/* global escape */
-'use strict'; // jshint ignore:line
-
-angular
-    .module('material-colorize', [
-        'ngRoute',
-        'lumx',
-        'hljs',
-        'Services'
-    ])
-    .controller('AppController', function(LxNotificationService)
-    {
-        $scope.dropdownAlert = function()
-        {
+angular.module('material-colorize', ['ngRoute', 'lumx', 'hljs', 'Services'])
+.controller('AppController', function(LxNotificationService) {
+    $scope.dropdownAlert = function() {
             LxNotificationService.info('Clicked!');
-        };
-
-    });
+    };
+});
