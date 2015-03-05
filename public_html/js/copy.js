ad7e93024f69335779e256075ac046d37b3ea89b
crutchcorn <crutchcorn@gmail.com>
2015-03-05 09:00:03 -0800

    Fixed ZeroClipboard

--- public_html/js/copy.js
+++ public_html/js/copy.js
@@ -1,13 +1,19 @@
 // main.js
-var client = new ZeroClipboard( document.getElementById("copy-button") );
+  var client = new ZeroClipboard( $('.clip_button') );
 
-client.on( "ready", function( readyEvent ) {
-  // alert( "ZeroClipboard SWF is ready!" );
+      client.on( 'ready', function(event) {
+        // console.log( 'movie is loaded' );
 
-  client.on( "aftercopy", function( event ) {
-    // `this` === `client`
-    // `event.target` === the element that was clicked
-    event.target.style.display = "none";
-    alert("Copied text to clipboard: " + event.data["text/plain"] );
-  } );
-} );
+        client.on( 'copy', function(event) {
+          event.clipboardData.setData('text/plain', event.target.innerHTML);
+        } );
+
+        client.on( 'aftercopy', function(event) {
+          console.log('Copied text to clipboard: ' + event.data['text/plain']);
+        } );
+      } );
+
+      client.on( 'error', function(event) {
+        // console.log( 'ZeroClipboard error of type "' + event.name + '": ' + event.message );
+        ZeroClipboard.destroy();
+      } );
