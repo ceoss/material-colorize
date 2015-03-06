e3b9facacd8be5b7949d9afd0b772e0796a3165e
Evelyn Hathaway <git@evelynhathaway.com>
2015-03-05 17:23:00 -0800

    Added Dialogs to Copy.js

--- public_html/js/copy.js
+++ public_html/js/copy.js
@@ -9,10 +9,11 @@ var client = new ZeroClipboard( $('.clip_button') );
 
 	  client.on( 'aftercopy', function(event) {
 		console.log('Copied text to clipboard: ' + event.data['text/plain']);
+		toast('Copied text to clipboard: ' + event.data['text/plain'], 4000)
 	  } );
 	} );
 
 	client.on( 'error', function(event) {
-	  // console.log( 'ZeroClipboard error of type "' + event.name + '": ' + event.message );
-	  ZeroClipboard.destroy();
+		toast('Flash Error: Copying may not work', 4000)
+		ZeroClipboard.destroy();
 	} );
