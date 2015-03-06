0a188c8f956d7d269c5eaaf34d97da43a897fb9d
Evelyn Hathaway <git@evelynhathaway.com>
2015-03-05 16:45:47 -0800

    Color Picker 1.2
    Fixed #9

--- public_html/js/copy.js
+++ public_html/js/copy.js
@@ -1,17 +1,18 @@
 ZeroClipboard.config({
-    forceHandCursor: true
+	forceHandCursor: true
 });
 
-var client = new ZeroClipboard($(".clip_button[data-clipboard-text]"));
+var client = new ZeroClipboard( $('.clip_button') );
 
-client.on("error", function(e) {
-    console.log("ERROR! [" + e.name + "] " + e.message);
-});
+	client.on( 'ready', function(event) {
+	  // console.log( 'movie is loaded' );
 
-client.on("ready", function(e) {
-    console.log("Ready!");
+	  client.on( 'aftercopy', function(event) {
+		console.log('Copied text to clipboard: ' + event.data['text/plain']);
+	  } );
+	} );
 
-    client.on("aftercopy", function(e) {
-        console.log((e.success["text/plain"] ? "Copied" : "FAILED to copy") + ": " + e.data["text/plain"]);
-    });
-});
+	client.on( 'error', function(event) {
+	  // console.log( 'ZeroClipboard error of type "' + event.name + '": ' + event.message );
+	  ZeroClipboard.destroy();
+	} );
