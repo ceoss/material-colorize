7d11f083cc12b68a53aaffc7625625dd49409eac
Evelyn Hathaway <git@evelynhathaway.com>
2015-03-05 17:44:52 -0800

    Convert Colors Rev 1

---
+++ public_html/js/formatSwitcher-convertColors.js
@@ -0,0 +1,11 @@
+function formatSwitcher(newFormat) {
+	if (newFormat==="hex-with-pound") {
+	} else if (newFormat==="hex-without-pound"){
+	} else if (newFormat==="hex-alpha-with-pound"){
+	} else if (newFormat==="hex-alpha-without-pound"){
+	} else if (newFormat==="rgb-with-formatting"){
+	} else if (newFormat==="rgb-without-formatting"){
+	} else if (newFormat==="rgba-with-formatting"){
+	} else if (newFormat==="rgba-without-formatting"){
+	};
+}
