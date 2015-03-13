baaa77390e198bed92d0666a3a8cb6c11472d5e5
Evelyn Hathaway <git@evelynhathaway.com>
2015-03-13 03:57:48 -0700

    ColorSwitcher.js for #19 Revised
    I used the ruby program too; changed it's name to something more to what
    it is

--- public_html/js/color-picker/colorSwitcher.js
+++ public_html/js/color-picker/colorSwitcher.js
@@ -1,53 +1,22 @@
-function colorSwitcher(newColor) {
-	ids = ["all", "red", "pink", "purple", "deep-purple", "indigo", "blue", "light-blue", "cyan", "teal", "green", "light-green", "lime", "yellow", "amber", "orange", "deep-orange", "brown", "grey", "blue-grey", "bnw", "color-ref-grey", "color-ref-grey-divider", "color-ref-bnw", "color-ref-bnw-divider"];
-	if (newColor==="all") {
-		values = ["initial", "initial", "initial", "initial", "initial", "initial", "initial", "initial", "initial", "initial", "initial", "initial", "initial", "initial", "initial", "initial", "initial", "initial", "initial", "initial", "initial", "none", "none", "none", "none"];
-	} else if (newColor==="red"){
-		values = ["none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="pink"){
-		values = ["none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="purple"){
-		values = ["none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="deep-purple"){
-		values = ["none", "none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="indigo"){
-		values = ["none", "none", "none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="blue"){
-		values = ["none", "none", "none", "none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="light-blue"){
-		values = ["none", "none", "none", "none", "none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="cyan"){
-		values = ["none", "none", "none", "none", "none", "none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="teal"){
-		values = ["none", "none", "none", "none", "none", "none", "none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="green"){
-		values = ["none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="light-green"){
-		values = ["none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="lime"){
-		values = ["none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="yellow"){
-		values = ["none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="amber"){
-		values = ["none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="orange"){
-		values = ["none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="deep-orange"){
-		values = ["none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="brown"){
-		values = ["none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "initial", "none", "none", "none", "none", "none", "none", "none"];
-	} else if (newColor==="grey"){
-		values = ["none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "initial", "none", "none", "initial", "initial", "none", "none"];
-	} else if (newColor==="blue-grey"){
-		values = ["none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "initial", "none", "none", "none", "none", "none"];
-	} else if (newColor==="bnw"){
-		values = ["none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "initial", "none", "none", "initial", "initial"];
-	};
-	displayProvider();
-}
-
-function displayProvider() {
-	for (var i = ids.length - 1; i >= 0; i--) {
-		document.getElementById(ids[i]).style.display = values[i];
+function colorSwitcher(color, all) {
+	var ids = ["all", "red", "pink", "purple", "deep-purple", "indigo", "blue", "light-blue", "cyan", "teal", "green", "light-green", "lime", "yellow", "amber", "orange", "deep-orange", "brown", "grey", "blue-grey", "bnw", "color-ref-grey", "color-ref-grey-divider", "color-ref-bnw", "color-ref-bnw-divider"]; // 25 different ids; four not for colors
+	var displayed = "initial"; // set all to 'initial' ('block' for cards)
+	if (all) { // debugging only
+		for (var i = ids.length - 1; i >= 0; i--) {
+			document.getElementById(ids[i]).style.display = displayed; 
+		};
+	} else { // end debugging
+		if (color === "grey") { //set titles and dividers
+			document.getElementById(ids[22]).style.display = displayed;
+			document.getElementById(ids[23]).style.display = displayed;
+		} else if (color === "bnw") {
+			document.getElementById(ids[24]).style.display = displayed;
+			document.getElementById(ids[25]).style.display = displayed;
+		} else {
+			for (var i = ids.length - 1; i >= 0; i--) {
+				document.getElementById(ids[i]).style.display = "none"; // hide all
+			};	
+		};
+		document.getElementById(color).style.display = displayed; // display chosen
 	};
 }
