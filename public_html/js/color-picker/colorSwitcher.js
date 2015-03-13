d014b007fa208c43a830948cf76126cc0abbc976
Evelyn Hathaway <git@evelynhathaway.com>
2015-03-13 05:59:24 -0700

    SetSwticher.js for #19 (Version 1)
    Title Tags are in the works. This code will error out, but work.

--- public_html/js/color-picker/colorSwitcher.js
+++ public_html/js/color-picker/colorSwitcher.js
@@ -1,27 +1,24 @@
 function colorSwitcher(color, all) {
-	var ids = ["all", "red", "pink", "purple", "deep-purple", "indigo", "blue", "light-blue", "cyan", "teal", "green", "light-green", "lime", "yellow", "amber", "orange", "deep-orange", "brown", "grey", "blue-grey", "bnw", "color-ref-grey", "color-ref-grey-divider", "color-ref-bnw", "color-ref-bnw-divider"]; // 25 different ids; four not for colors
-	var displayed = "initial"; // set all to 'initial' ('block' for cards)
+	var ids = ["all", "red", "pink", "purple", "deep-purple", "indigo", "blue", "light-blue", "cyan", "teal", "green", "light-green", "lime", "yellow", "amber", "orange", "deep-orange", "brown", "grey", "blue-grey", "bnw", "color-ref", "color-ref-divider"]; // 23 different ids; two not for colors
 	if (all) { // debugging only
 		for (var i = ids.length - 1; i >= 0; i--) {
-			document.getElementById(ids[i]).style.display = displayed; 
+			document.getElementById(ids[i]).style.display = "block"; 
 		};
 	} else { // end debugging
 		for (var i = ids.length - 1; i >= 0; i--) {
 			document.getElementById(ids[i]).style.display = "none"; // hide all
-		};	
-		if (color === "grey") { //set titles and dividers
-			document.getElementById(ids[21]).style.display = displayed;
-			document.getElementById(ids[22]).style.display = displayed;
-		} else if (color === "bnw") {
-			document.getElementById(ids[23]).style.display = displayed;
-			document.getElementById(ids[24]).style.display = displayed;
+		};
+		if (color === "grey" || color === "bnw") { // set titles and dividers
+			document.getElementById(ids[21]).style.display = "block";
+			document.getElementById(ids[22]).style.display = "block";
 		} else if (color === "all") {
 			for (var i = ids.length - 1; i >= 0; i--) {
-				document.getElementById(ids[i]).style.display = displayed; // display all
-			};	
-		} else {
-			// you're a pretty color :D (well and you, brown)
+				document.getElementById(ids[i]).style.display = "block"; // display all
+			};
+			document.getElementById(ids[21]).style.display = "none"; // hide ref title
+			document.getElementById(ids[22]).style.display = "none"; // hide ref divider
+		} else { // you're a pretty color :D (well and you, brown)
 		};
-		document.getElementById(color).style.display = displayed; // display chosen
+		document.getElementById(color).style.display = "block"; // display chosen
 	};
 }
