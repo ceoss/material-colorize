d82eb8e1947da0f9e7f54f3d584e47e4f5c1866c
Evelyn Hathaway <git@evelynhathaway.com>
2016-02-19 18:00:40 -0800

    Convert Function

--- src/js/script.js
+++ src/js/script.js
@@ -1223,3 +1223,39 @@ function colorRemove() {
 colorAdd(palette);
 
 new Clipboard("i.clipboard");
+
+function hexToRgb(hex) {
+	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+	hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+		return r + r + g + g + b + b;
+	});
+
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	return result ? {
+		r: parseInt(result[1], 16),
+		g: parseInt(result[2], 16),
+		b: parseInt(result[3], 16)
+	} : null;
+}
+
+function convert(color) {
+	var colorRgb = hexToRgb(color);
+	var diff = {};
+	var mins = {};
+	var keys = Object.keys(palette);
+	for (var array = 0; array <= keys.length - 1; array++) {
+		for (var color = 0; color <= palette[keys[array]].length - 1; color++) {
+			var colorObject = palette[keys[array]][color];
+			if (!diff[keys[array]]) {
+				diff[keys[array]] = [];
+			}
+			var materialRgb = hexToRgb(colorObject.hex);
+			diff[keys[array]].push(Math.abs(materialRgb.r - colorRgb.r) + Math.abs(materialRgb.g - colorRgb.g) + Math.abs(materialRgb.b - colorRgb.b));
+		}
+		mins[keys[array]] = {
+			"index": diff[keys[array]].indexOf(Math.min.apply(Math, diff[keys[array]])),
+			"diff": Math.min.apply(Math, diff[keys[array]])
+		};
+	}
+	return mins;
+}
