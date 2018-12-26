cdf05816d5a749778ff2f94881619b6e9d96dcc4
Crutchcorn <crutchcorn@gmail.com>
2018-12-26 01:13:50 -0700

    Fix color on manifest, make theme match

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import Tabs from './components/Tabs';
-import PaletteListView from './views/PaletteListView';
+import React from "react";
+import Tabs from "./components/Tabs";
+import PaletteListView from "./views/PaletteListView";
 import MatchColorView from "./views/MatchColorView";
 import ImageExtractView from "./views/ImageExtractView";
-import Colorize from '@material-ui/icons/ColorizeOutlined';
-import CompareArrows from '@material-ui/icons/CompareArrowsOutlined';
-import Palette from '@material-ui/icons/PaletteOutlined';
+import Colorize from "@material-ui/icons/ColorizeOutlined";
+import CompareArrows from "@material-ui/icons/CompareArrowsOutlined";
+import Palette from "@material-ui/icons/PaletteOutlined";
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core/styles";
+import blue from "@material-ui/core/colors/blue";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue
+  },
+  typography: {useNextVariants: true},
+});
 
 export default function App() {
   return (
-    <Tabs>
-      <ImageExtractView tabLabel="Extract" path="/" tabIcon={<Palette/>} showOverflow={true}/>
-      <PaletteListView  tabLabel="Palette" path="/palette" tabIcon={<Colorize/>}/>
-      <MatchColorView tabLabel="Match" path="/match" tabIcon={<CompareArrows/>} showOverflow={true}/>
-    </Tabs>
+    <MuiThemeProvider theme={theme}>
+      <Tabs>
+        <ImageExtractView tabLabel="Extract" path="/" tabIcon={<Palette/>} showOverflow={true}/>
+        <PaletteListView tabLabel="Palette" path="/palette" tabIcon={<Colorize/>}/>
+        <MatchColorView tabLabel="Match" path="/match" tabIcon={<CompareArrows/>} showOverflow={true}/>
+      </Tabs>
+    </MuiThemeProvider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
